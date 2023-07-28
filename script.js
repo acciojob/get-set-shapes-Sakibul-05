89b5e02663c6fb29220ed6be00a5a9d7e554dd91
Sakibul-05 <110473516+Sakibul-05@users.noreply.github.com>
2023-07-28 19:24:18 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,29 +1,32 @@
 //complete this code
 class Rectangle {
-	constructor(width, height){
-		this._width= width
-		this._height=height
-	}
-	get width(){
-		return this._width
-	}
-	get height(){
-		return this._height
-	}
-	getArea(){
-		let area = this._width*this._height
-		return area
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
+  get height() {
+    return this._height;
+  }
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	constructor(sq_s1,sq_s2){
-		super(sq_s1,sq_s2)
-		 
-	}
-	getPerimeter(){
-		return 2*(this._width+this._height)
-	}
+  constructor(side) {
+    // Since square has equal sides, we only need to pass one argument (side) to the constructor.
+    super(side, side);
+  }
+
+  getPerimeter() {
+    return 4 * this.width; // Since all sides are equal in a square, perimeter = 4 * side.
+  }
 }
 
 // Do not change the code below this line
